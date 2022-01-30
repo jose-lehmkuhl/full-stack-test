a9aa5d33b062db315b7d1294ebe5da5e91cf7a03
José Lehmkuhl <josevicentekoerich@gmail.com>
2022-01-30 19:03:55 -0300

    initializing db on server startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const startServer = async () => {
+  const db = require('./db');
+
   const app = express();
   app.use(express.json());
 
+  await db.sequelize.sync();
   app.listen(3001, () => {
     console.log('server running');
   });
