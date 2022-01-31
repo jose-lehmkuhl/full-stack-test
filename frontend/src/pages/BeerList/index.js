cf2c665752a8ecbebead46876b6e7c1460322dd5
José Lehmkuhl <josevicentekoerich@gmail.com>
2022-01-30 21:23:20 -0300

    Add pagination

--- frontend/src/pages/BeerList/index.js
+++ frontend/src/pages/BeerList/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext, useState } from 'react'
 import Grid from '@mui/material/Grid'
 import Container from '@mui/material/Container'
 import CircularProgress from '@mui/material/CircularProgress'
+import Pagination from '@mui/material/Pagination'
 
 import { AuthContext } from '../../context/AuthContext'
 import api from '../../services/api'
@@ -12,6 +13,7 @@ const BeerList = () => {
   const { token } = useContext(AuthContext)
   const [beers, setBeers] = useState([])
   const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
 
   const getAllBeers = async () => {
     const getBeers = async (page = 1, beers = []) => {
@@ -35,13 +37,19 @@ const BeerList = () => {
     getAllBeers()
   }, [])
 
+  const totalPages = Math.ceil(beers.length / 12)
   return (
           <Container component="main">
               <Grid container spacing={2} justifyContent='center' >
               {loading
                 ? <CircularProgress size={14} />
-                : beers.slice(0, 10).map(beer => <Beer beer={beer} key={beer.id}/>)
-                }
+                : beers.slice((page - 1) * 12, ((page - 1) * 12) + 12).map(beer => <Beer beer={beer} key={beer.id}/>)
+              }
+               <Grid item xs={12}>
+                    <Grid container spacing={2} justifyContent='center' >
+                        {!loading && <Pagination size='large' count={totalPages} color="primary" page={page} onChange={(event, page) => setPage(page)} />}
+                    </Grid>
+                </Grid>
               </Grid>
           </Container>
   )
